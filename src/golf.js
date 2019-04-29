316356d81467d77c2b6574b119c73aaf0e4bc173
Kendra Tate <tateconsult@hotmail.com>
2019-04-29 17:26:30 -0600

    fix Golf api call

--- src/golf.js
+++ src/golf.js
@@ -169,7 +169,7 @@ function buildTable(){
 
 
 function selectCourse() {
-    $.post("http://golf-courses-api.herokuapp.com/courses/", position, function (data, status) {
+    $.post("golf-courses-api.herokuapp.com/courses/", position, function (data, status) {
         closeCourses = JSON.parse(data);
         $("#courseselect").append("<option disabled selected>Select a Golf Course</option>");
         for (var p in closeCourses.courses) {
@@ -182,7 +182,7 @@ function selectCourse() {
 }
 
 function getCourse(courseID){
-    $.get("https://golf-courses-api.herokuapp.com/courses/" + courseID, function(data,status) {
+    $.get("golf-courses-api.herokuapp.com/courses/" + courseID, function(data,status) {
         activeCourse = JSON.parse(data);
     }).then(
         function(parseCourse){
